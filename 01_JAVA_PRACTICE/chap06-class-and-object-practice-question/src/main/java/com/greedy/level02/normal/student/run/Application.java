d552487d7ccc8dc72f337bec2faa5cd37d569579
inji-yeon <145536243+inji-yeon@users.noreply.github.com>
2023-10-14 18:05:33 +0900

    Add files via upload

--- 01_JAVA_PRACTICE/chap06-class-and-object-practice-question/src/main/java/com/greedy/level02/normal/student/run/Application.java
+++ 01_JAVA_PRACTICE/chap06-class-and-object-practice-question/src/main/java/com/greedy/level02/normal/student/run/Application.java
@@ -1,4 +1,17 @@
 package com.greedy.level02.normal.student.run;
 
+import com.greedy.level02.normal.student.model.vo.StudentVO;
+
+import java.util.Scanner;
+
 public class Application {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        StudentVO student = new StudentVO(sc.nextInt(), sc.nextInt(), sc.next(), sc.nextDouble(), sc.next().charAt(0));
+
+        student.printInformation();
+    }
 }
